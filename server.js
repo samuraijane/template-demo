b3259eda6f72cc3c04a24a3d08a9339163fef43b
Matthew Day <matt@matthewday.net>
2021-10-12 18:57:50 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { application } = require('express');
+const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
+const morgan = require('morgan');
+
+const server = express();
+
+server.get('/heartbeat', (req, res) => {
+  res.json({
+    "is": "working"
+  })
+});
+
+
+server.use('/', express.static(__dirname + '/public'));
+
+
+// template rendering
+server.engine('html', es6Renderer);
+server.set('views', 'views');
+server.set('view engine', 'html');
+
+
+server.get('/', (req, res) => {
+  res.render('landing', {
+    partials: {
+      footer: 'partials/footer',
+      header: 'partials/header'
+    }
+  });
+})
+
+const { PORT } = process.env;
+
+server.listen(PORT, () => {
+  console.log(`The server is running at ${PORT}`);
+})
